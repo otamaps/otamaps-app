31538b6c0770deb7f1169b1b96e53ac91358df46
Rene Saarikko <renesaarikko@gmail.com>
2025-08-05 23:32:28 +0300

    feat: integrate feature store and enhance feature handling in map component

--- app/welcome/(pre)/_layout.tsx
+++ app/welcome/(pre)/_layout.tsx
@@ -4,7 +4,14 @@ export default function WelcomeLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen 
+        name="login" 
+        options={{ 
+          headerShown: false,
+          presentation: 'card',
+          gestureEnabled: false 
+        }} 
+      />
     </Stack>
   );
 }
